aff4898dbe5e0c813daef08f593f768e99591a2b
xx <shenxiangxiang@gmail.com>
2026-03-26 21:43:57 +0800

    khanhduytran0 patch

--- BaseBin/systemhook/src/main.c
+++ BaseBin/systemhook/src/main.c
@@ -17,6 +17,7 @@
 #include "private.h"
 
 bool gFullyDebugged = false;
+bool gHideTracedFlag = true;
 static void *gLibSandboxHandle;
 char *JB_BootUUID = NULL;
 char *JB_RootPath = NULL;
@@ -99,6 +100,13 @@ void *dyld_dlsym_hook(void *dyld, void *handle, const char *name)
 
 int ptrace_hook(int request, pid_t pid, caddr_t addr, int data)
 {
+	bool did_preflight = false;
+    if (request == PT_ATTACHEXC || request == PT_ATTACH) {
+		if (jbclient_platform_clear_process_noattach(pid, true, false) == 0) {
+			did_preflight = true;
+		}
+    }
+
 	int r = syscall(SYS_ptrace, request, pid, addr, data);
 
 	// ptrace works on any process when the caller is unsandboxed,
@@ -110,6 +118,11 @@ int ptrace_hook(int request, pid_t pid, caddr_t addr, int data)
 	if (r == 0 && (request == PT_ATTACHEXC || request == PT_ATTACH)) {
 		jbclient_platform_set_process_debugged(pid, true);
 		jbclient_platform_set_process_debugged(getpid(), true);
+		
+	}
+	if (did_preflight) {
+		bool ptrace_success = (r == 0);
+		jbclient_platform_clear_process_noattach(pid, false, ptrace_success ? gHideTracedFlag : false);
 	}
 
 	return r;
